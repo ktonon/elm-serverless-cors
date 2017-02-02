c402a19f88ca4d017bc4677087fc13f653bab75e
Kevin Tonon <kevin@betweenconcepts.com>
2017-02-01 21:51:29 -0500

    Middleware, decoders, and tests. Still need to add docs

--- src/Serverless/Cors.elm
+++ src/Serverless/Cors.elm
@@ -1,27 +1,292 @@
-module Serverless.Cors exposing (..)
+module Serverless.Cors
+    exposing
+        ( Config
+        , Reflectable(..)
+        , configDecoder
+        , methodsDecoder
+        , reflectableDecoder
+        , cors
+        , allowOrigin
+        , exposeHeaders
+        , maxAge
+        , allowCredentials
+        , allowMethods
+        , allowHeaders
+        )
 
-{-| This small module contains a single function for enabling CORS.
+{-| CORS Middleware for elm-serverless.
 
-@docs cors
+There are two ways to use the middleware:
+
+* use `cors` passing in a `Config` (likely decoded from JSON using configDecoder)
+* call individual headers like `allowOrigin` and `allowMethods` separately
+
+## Types
+
+@docs Config, Reflectable
+
+## Decoders
+
+@docs configDecoder, methodsDecoder, reflectableDecoder
+
+## Middleware
+
+@docs cors, allowOrigin, exposeHeaders, maxAge, allowCredentials, allowMethods, allowHeaders
+
 -}
 
-import Serverless.Conn as Conn
-import Serverless.Conn.Types exposing (Method)
+import Dict
+import Json.Decode exposing (andThen, bool, fail, int, list, oneOf, string, succeed, Decoder)
+import Json.Decode.Pipeline exposing (decode, optional)
+import Serverless.Conn exposing (..)
+import Serverless.Conn.Types exposing (..)
 import Serverless.Types exposing (Conn)
+import Toolkit.Helpers exposing (maybeList)
 
 
-{-| Add cors headers to the response.
+-- TYPES
 
-    pipeline
-        |> plug (cors "*" [ GET, OPTIONS ])
+
+{-|
 -}
-cors : String -> List Method -> Conn config model -> Conn config model
-cors origin methods =
-    (Conn.header ( "access-control-allow-origin", origin ))
-        >> (Conn.header
-                ( "access-control-allow-headers"
-                , methods
-                    |> List.map toString
-                    |> String.join ", "
+type alias Config =
+    { origin : Reflectable (List String)
+    , expose : List String
+    , maxAge : Int
+    , credentials : Bool
+    , methods : List Method
+    , headers : Reflectable (List String)
+    }
+
+
+{-|
+-}
+type Reflectable a
+    = ReflectRequest
+    | Exactly a
+
+
+
+-- DECODERS
+
+
+{-|
+-}
+configDecoder : Decoder Config
+configDecoder =
+    decode Config
+        |> optional "origin" reflectableDecoder (Exactly [])
+        |> optional "expose" stringListDecoder []
+        |> optional "maxAge" positiveIntDecoder 0
+        |> optional "credentials" truthyDecoder False
+        |> optional "methods" methodsDecoder []
+        |> optional "headers" reflectableDecoder (Exactly [])
+
+
+{-|
+-}
+reflectableDecoder : Decoder (Reflectable (List String))
+reflectableDecoder =
+    stringListDecoder
+        |> andThen
+            (\strings ->
+                if strings == [ "*" ] then
+                    succeed ReflectRequest
+                else
+                    strings |> Exactly |> succeed
+            )
+
+
+positiveIntDecoder : Decoder Int
+positiveIntDecoder =
+    int
+        |> andThen
+            (\val ->
+                if val < 0 then
+                    fail "negative value when zero or positive was expected"
+                else
+                    succeed val
+            )
+
+
+truthyDecoder : Decoder Bool
+truthyDecoder =
+    oneOf
+        [ bool
+        , int |> andThen (\val -> val /= 0 |> succeed)
+        , string |> andThen (\val -> not (String.isEmpty val) |> succeed)
+        ]
+
+
+stringListDecoder : Decoder (List String)
+stringListDecoder =
+    oneOf
+        [ list string
+        , string |> andThen (String.split "," >> succeed)
+        ]
+
+
+{-|
+-}
+methodsDecoder : Decoder (List Method)
+methodsDecoder =
+    stringListDecoder
+        |> andThen
+            (\strings ->
+                case
+                    strings
+                        |> List.map
+                            (\w ->
+                                case w |> String.toLower of
+                                    "get" ->
+                                        Just GET
+
+                                    "post" ->
+                                        Just POST
+
+                                    "put" ->
+                                        Just PUT
+
+                                    "delete" ->
+                                        Just DELETE
+
+                                    "options" ->
+                                        Just OPTIONS
+
+                                    _ ->
+                                        Nothing
+                            )
+                        |> maybeList
+                of
+                    Just methods ->
+                        succeed methods
+
+                    Nothing ->
+                        fail
+                            ("Invalid CORS methods: "
+                                ++ (strings |> String.join ",")
+                            )
+            )
+
+
+
+-- MIDDLEWARE
+
+
+{-|
+-}
+cors : Config -> Conn config model -> Conn config model
+cors config =
+    allowOrigin config.origin
+        >> exposeHeaders config.expose
+        >> maxAge config.maxAge
+        >> allowCredentials config.credentials
+        >> allowMethods config.methods
+        >> allowHeaders config.headers
+
+
+{-|
+-}
+allowOrigin : Reflectable (List String) -> Conn config model -> Conn config model
+allowOrigin origin conn =
+    case origin of
+        ReflectRequest ->
+            conn
+                |> header
+                    ( "access-control-allow-origin"
+                    , conn.req.headers
+                        |> Dict.fromList
+                        |> Dict.get "origin"
+                        |> Maybe.withDefault "*"
+                    )
+
+        Exactly origins ->
+            if origins |> List.isEmpty then
+                conn
+            else
+                conn
+                    |> header
+                        ( "access-control-allow-origin"
+                        , origins |> String.join ","
+                        )
+
+
+{-|
+-}
+exposeHeaders : List String -> Conn config model -> Conn config model
+exposeHeaders headers conn =
+    if headers |> List.isEmpty then
+        conn
+    else
+        conn
+            |> header
+                ( "access-control-expose-headers"
+                , headers |> String.join ","
                 )
-           )
+
+
+{-|
+-}
+maxAge : Int -> Conn config model -> Conn config model
+maxAge age conn =
+    if age > 0 then
+        conn
+            |> header
+                ( "access-control-max-age"
+                , age |> toString
+                )
+    else
+        conn
+
+
+{-|
+-}
+allowCredentials : Bool -> Conn config model -> Conn config model
+allowCredentials allow conn =
+    if allow then
+        conn |> header ( "access-control-allow-credentials", "true" )
+    else
+        conn
+
+
+{-|
+-}
+allowMethods : List Method -> Conn config model -> Conn config model
+allowMethods methods conn =
+    if methods |> List.isEmpty then
+        conn
+    else
+        conn
+            |> header
+                ( "access-control-allow-methods"
+                , methods |> List.map toString |> String.join ","
+                )
+
+
+{-|
+-}
+allowHeaders : Reflectable (List String) -> Conn config model -> Conn config model
+allowHeaders headers conn =
+    case headers of
+        ReflectRequest ->
+            case
+                conn.req.headers
+                    |> Dict.fromList
+                    |> Dict.get "access-control-request-headers"
+            of
+                Just requestHeaders ->
+                    conn
+                        |> header
+                            ( "access-control-allow-headers"
+                            , requestHeaders
+                            )
+
+                Nothing ->
+                    conn
+
+        Exactly h ->
+            conn
+                |> header
+                    ( "access-control-allow-headers"
+                    , h |> String.join ","
+                    )
---
+++ tests/Tests.elm
@@ -0,0 +1,205 @@
+module Tests exposing (..)
+
+import Dict
+import ElmTestBDDStyle exposing (..)
+import Expect exposing (..)
+import Serverless.Conn.Types exposing (..)
+import Serverless.Cors exposing (..)
+import Serverless.Types exposing (PipelineState(..), Sendable(..))
+import Test exposing (..)
+import Test.Extra exposing (..)
+
+
+all : Test
+all =
+    describe "Serverless.Cors"
+        [ decoderTests
+        , middlewareTests
+        ]
+
+
+testHeader : Conn -> List ( String, String ) -> Test
+testHeader conn headers =
+    it ("headers: " ++ (headers |> toString)) <|
+        case conn.resp of
+            Unsent resp ->
+                expect
+                    resp.headers
+                    to
+                    equal
+                    headers
+
+            Sent ->
+                fail ""
+
+
+middlewareTests : Test
+middlewareTests =
+    describe "CORS Middleware"
+        [ describe "allowOrigin"
+            [ testHeader
+                (conn |> allowOrigin ReflectRequest)
+                [ ( "access-control-allow-origin", "*" ) ]
+            , testHeader
+                (connWithHeader "origin" "foo.bar.com"
+                    |> allowOrigin ReflectRequest
+                )
+                [ ( "access-control-allow-origin", "foo.bar.com" ) ]
+            , testHeader
+                (conn
+                    |> allowOrigin (Exactly [])
+                )
+                []
+            , testHeader
+                (conn
+                    |> allowOrigin (Exactly [ "foo.bar.com" ])
+                )
+                [ ( "access-control-allow-origin", "foo.bar.com" ) ]
+            , testHeader
+                (conn |> allowHeaders ReflectRequest)
+                []
+            , testHeader
+                (connWithHeader "access-control-request-headers" "foo,bar,car"
+                    |> allowHeaders ReflectRequest
+                )
+                [ ( "access-control-allow-headers", "foo,bar,car" ) ]
+            , testHeader
+                (conn |> allowMethods [ GET, OPTIONS ])
+                [ ( "access-control-allow-methods", "GET,OPTIONS" ) ]
+            , testHeader
+                (conn |> allowCredentials False)
+                []
+            , testHeader
+                (conn |> allowCredentials True)
+                [ ( "access-control-allow-credentials", "true" ) ]
+            ]
+        ]
+
+
+decoderTests : Test
+decoderTests =
+    describe "Decoders"
+        [ describeDecoder "configDecoder"
+            configDecoder
+            [ ( "{}", DecodesTo nullConfig )
+            , ( """{ "origin": "*" }"""
+              , DecodesTo { nullConfig | origin = ReflectRequest }
+              )
+            , ( """{ "origin": ["x.y.z", "a.b.c"] }"""
+              , DecodesTo { nullConfig | origin = Exactly [ "x.y.z", "a.b.c" ] }
+              )
+            , ( """{ "methods": ["get", "options"] }"""
+              , DecodesTo { nullConfig | methods = [ GET, OPTIONS ] }
+              )
+            , ( """{ "expose": "foo-bar,car" }"""
+              , DecodesTo { nullConfig | expose = [ "foo-bar", "car" ] }
+              )
+            , ( """{ "maxAge": 8 }"""
+              , DecodesTo { nullConfig | maxAge = 8 }
+              )
+            , ( """{ "maxAge": -1 }"""
+              , FailsToDecode
+              )
+            , ( """{ "credentials": true }"""
+              , DecodesTo { nullConfig | credentials = True }
+              )
+            , ( """{ "credentials": false }"""
+              , DecodesTo nullConfig
+              )
+            , ( """{ "credentials": 1 }"""
+              , DecodesTo { nullConfig | credentials = True }
+              )
+            , ( """{ "credentials": 0 }"""
+              , DecodesTo nullConfig
+              )
+            , ( """{ "credentials": "" }"""
+              , DecodesTo nullConfig
+              )
+            , ( """{ "credentials": "true" }"""
+              , DecodesTo { nullConfig | credentials = True }
+              )
+            , ( """{ "headers": "*" }"""
+              , DecodesTo { nullConfig | headers = ReflectRequest }
+              )
+            , ( """{ "headers": ["foo-bar", "car"] }"""
+              , DecodesTo { nullConfig | headers = Exactly [ "foo-bar", "car" ] }
+              )
+            ]
+        , describeDecoder "methodsDecoder"
+            methodsDecoder
+            [ ( "[]", DecodesTo [] )
+            , ( "\"get\"", DecodesTo [ GET ] )
+            , ( "\"post\"", DecodesTo [ POST ] )
+            , ( "\"put\"", DecodesTo [ PUT ] )
+            , ( "\"delete\"", DecodesTo [ DELETE ] )
+            , ( "\"options\"", DecodesTo [ OPTIONS ] )
+            , ( "\"GeT\"", DecodesTo [ GET ] )
+            , ( "\"get,post\"", DecodesTo [ GET, POST ] )
+            , ( "[\"get\", \"post\"]", DecodesTo [ GET, POST ] )
+            ]
+        , describeDecoder "reflectableDecoder"
+            reflectableDecoder
+            [ ( "[]", DecodesTo (Exactly []) )
+            , ( "\"*\"", DecodesTo ReflectRequest )
+            , ( "\"x.y.z\"", DecodesTo (Exactly [ "x.y.z" ]) )
+            , ( "\"x.y.z,a.b.c\"", DecodesTo (Exactly [ "x.y.z", "a.b.c" ]) )
+            , ( "[\"x.y.z\", \"a.b.c\"]", DecodesTo (Exactly [ "x.y.z", "a.b.c" ]) )
+            ]
+        ]
+
+
+
+-- HELPERS
+
+
+nullConfig : Config
+nullConfig =
+    Config
+        (Exactly [])
+        []
+        0
+        False
+        []
+        (Exactly [])
+
+
+type alias Conn =
+    Serverless.Types.Conn Bool Bool
+
+
+connWithHeader : String -> String -> Conn
+connWithHeader key value =
+    let
+        req =
+            conn.req
+    in
+        { conn | req = { req | headers = [ ( key, value ) ] } }
+
+
+conn : Conn
+conn =
+    Serverless.Types.Conn
+        Processing
+        False
+        (Request
+            ""
+            NoBody
+            []
+            ""
+            GET
+            ""
+            80
+            (Ip4 ( 127, 0, 0, 1 ))
+            (Http Insecure)
+            ""
+            []
+        )
+        (Unsent
+            (Response
+                NoBody
+                Utf8
+                []
+                InvalidStatus
+            )
+        )
+        False
